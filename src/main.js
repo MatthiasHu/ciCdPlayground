3ec7f4fcd523b9389cdd6db60ab94cfa56c3399f
Matthias Ritter <matthias.ritter@tngtech.com>
2025-05-16 13:49:17 +0200

    some edit

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "Christof Happ",
+    name: "Matthias Ritter",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["red, green and yellow", "fast feedback", "iterative development process"],
+    associations: ["great stuff"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins",
+    tools: ["Jenkins"],
     // What do you want to learn in this workshop?
-    expectations: ["How to improve the workshop.", "Which CI/CD Tools do you use in your projects?"],
+    expectations: [],
   },
 });
